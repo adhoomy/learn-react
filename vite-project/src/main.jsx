6ddf8d28aef50c58ad9d54500e30084784463851
Adham Mustafa <adhoomy.mustafa@gmail.com>
2025-09-08 21:45:56 -0400

    Finished lessons 1-10.

---
+++ vite-project/src/main.jsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+    <img src="./src/assets/react.svg" alt="React Logo" />
+);
